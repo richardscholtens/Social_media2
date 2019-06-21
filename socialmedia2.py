58eb33f2c0182b43d83efb4f58ed41fab9d15810
Richard Scholtens <richardscholtens2@gmail.com>
2019-06-21 14:08:01 +0200

    nieuwe question

--- socialmedia2.py
+++ socialmedia2.py
@@ -32,9 +32,12 @@ def get_thread_lengths(lst):
     return all_length
 
 
-def calculate_mean(lst):
+def calculate_log_mean(lst):
     return np.log(sum(lst)/len(lst))
-    #return sum(lst)/len(lst)
+
+
+def calculate_mean(lst):
+    return sum(lst)/len(lst)
 
 
 def check_time(time, counter):
@@ -47,7 +50,7 @@ def check_time(time, counter):
     return counter
 
 
-def scatter_plot(info, xlabel, ylabel):
+def scatter_plot(info, xlabel, ylabel, title):
     """Sequential coherence"""
     interface1_thread, interface1_conv, interface2_thread, interface2_conv = zip(*info)
     plt.scatter(interface1_thread, interface1_conv, color='r')
@@ -60,41 +63,33 @@ def scatter_plot(info, xlabel, ylabel):
     p2 = np.poly1d(z2)
     plb.plot(interface1_thread, p1(interface1_thread), 'm-', color='g', label='Single interface 1')
     plb.plot(interface2_thread, p2(interface2_thread), 'm-', color='r', label='Double interface 2')
+    plt.title(title)
     plt.legend()
     plt.show()
 
 
-def bar_plot(bars1, bars2, xlabel, ylabel):
+def bar_plot(bars1, bars2, ylabel1, xlabels, title):
+
     # width of the bars
     barWidth = 0.3
-     
-
-
-    print(bars1)
-    # Choose the height of the blue bars
-    bars1 = [10, 9, 2]
-    # Choose the height of the cyan bars
-    bars2 = [10.8, 9.5, 4.5]
-     
 
     # The x position of bars
     r1 = np.arange(len(bars1))
     r2 = [x + barWidth for x in r1]
      
     # Create blue bars
-    plt.bar(r1, bars1, width = barWidth, color = 'blue', edgecolor = 'black', capsize=7, label='poacee')
+    plt.bar(r1, bars1, width = barWidth, color = 'blue', edgecolor = 'black', capsize=7, label='Interface 1')
      
     # Create cyan bars
-    plt.bar(r2, bars2, width = barWidth, color = 'cyan', edgecolor = 'black', capsize=7, label='sorgho')
+    plt.bar(r2, bars2, width = barWidth, color = 'cyan', edgecolor = 'black', capsize=7, label='Interface 2')
      
     # general layout
-    plt.xticks([r + barWidth for r in range(len(bars1))], ['cond_A', 'cond_B', 'cond_C'])
+    plt.xticks([r + barWidth for r in range(len(bars1))], ['Compound', 'Positive', 'Neutral', 'Negative'])
     plt.ylabel('height')
     plt.legend()
-     
+    plt.title(title)
     # Show graphic
     plt.show()
-
 
 
 def interface_splitter(matrix):
@@ -158,13 +153,15 @@ def cosine_similarity_and_sentiment(matrix):
     vect = TfidfVectorizer(min_df=1)
     tfidf = vect.fit_transform([s_conv, o_conv])
     lst = (tfidf * tfidf.T).A
+    all_comp, all_pos, all_neu, all_neg = sentimentfinder(s_conv + o_conv)
     compound1, pos1, neu1, neg1 = sentimentfinder(s_conv)
     compound2, pos2, neu2, neg2 = sentimentfinder(o_conv)
 
+    all_tup = (all_comp, all_pos, all_neu, all_neg)
     s_tup = (compound1, pos1, neu1, neg1)
     o_tup = (compound2, pos2, neu2, neg2)
 
-    return lst[0][1], s_tup, o_tup
+    return lst[0][1], all_tup, s_tup, o_tup
 
 
 def sentimentfinder(string):
@@ -202,7 +199,7 @@ def main():
             e2 = np.array(f2.split('¦'))
             e3 = np.array(f3.split('¦'))
             e4 = np.array(f4.split('¦'))
-            print(len(e1), len(e2), len(e3[2:]), len(e4))
+            #print(len(e1), len(e2), len(e3[2:]), len(e4))
 
             #m = np.column_stack((e1, e2, e3[2:], e4))
             #matrix_lst.append(m)
@@ -222,22 +219,21 @@ def main():
     time_means2 = []
     sim_lst1 = []
     sim_lst2 = []
-    compound_lst1 = []
-    compound_lst2 = []
+    sent_lst1 = []
+    sent_lst2 = []
 
     for i in matrix_lst2:
         interface1, interface2 = interface_splitter(i)
         thr_length1, time_length1 = thread_length_time(interface1)
         thr_length2, time_length2 = thread_length_time(interface2)
         conversations.append((thr_length1, thr_length2))
-        similiarity1, sent_s1, sent_1 = cosine_similarity_and_sentiment(interface1)
-        similiarity2, sent_s2, sent_2 = cosine_similarity_and_sentiment(interface2)
+        similiarity1, sent_s1, sent_o1 = cosine_similarity_and_sentiment(interface1)
+        similiarity2, sent_s2, sent_o2 = cosine_similarity_and_sentiment(interface2)
 
-        mean1 = calculate_mean(thr_length1)
-        mean2 = calculate_mean(thr_length2)
-        mean3 = calculate_mean(time_length1)
-        mean4 = calculate_mean(time_length2)
-
+        mean1 = calculate_log_mean(thr_length1)
+        mean2 = calculate_log_mean(thr_length2)
+        mean3 = calculate_log_mean(time_length1)
+        mean4 = calculate_log_mean(time_length2)
 
         thr_means1.append(mean1)
         thr_means2.append(mean2)
@@ -245,11 +241,26 @@ def main():
         time_means2.append(mean4)
         sim_lst1.append(similiarity1)
         sim_lst2.append(similiarity2)
-        compound_lst1.append(sent_s1[0])
-        compound_lst2.append(sent_s1[0])
+        sent_lst1.append(sent_s1)
+        sent_lst2.append(sent_s2)
 
     turns_lst1 = []
     turns_lst2 = []
+
+    compound1, pos1, neu1, neg1 = zip(*sent_lst1)
+    compound2, pos2, neu2, neg2 = zip(*sent_lst2)
+
+    compound_means1 = calculate_mean(compound1)
+    pos_means1 = calculate_mean(pos1)
+    neu_means1 = calculate_mean(neu1)
+    neg_means1 = calculate_mean(neg1)
+    q4_interface1 = (compound_means1, pos_means1, neu_means1, neg_means1)
+
+    compound_means2 = calculate_mean(compound2)
+    pos_means2 = calculate_mean(pos2)
+    neu_means2 = calculate_mean(neu2)
+    neg_means2 = calculate_mean(neg2)
+    q4_interface2 = (compound_means2, pos_means2, neu_means2, neg_means2)
 
 
     for i1, i2 in conversations:
@@ -264,11 +275,12 @@ def main():
     q2 = structure_data(thr_means1, time_means1, thr_means2, time_means2)
     q3 = structure_data(sim_lst1, thr_means1, sim_lst2, thr_means2)
 
-    scatter_plot(q1, 'Mean thread length (log)', 'Conversation length')
-    scatter_plot(q2, 'Mean thread length (log)', 'Mean time length (log)')
-    scatter_plot(q3, 'Cosine similiarity', 'Mean thread length (log)')
-    scatter_plot(q3, 'Cosine similiarity', 'Mean thread length (log)')
-    bar_plot(compound1, compound_lst2)
+
+    scatter_plot(q1, 'Mean thread length (log)', 'Conversation length', 'Sequence coherence')
+    scatter_plot(q2, 'Mean thread length (log)', 'Mean time length (log)', 'Thread length vs Time')
+    scatter_plot(q3, 'Cosine similiarity', 'Mean thread length (log)', 'Cosine similiarity vs Thread length')
+
+    bar_plot(q4_interface1, q4_interface2, "Mean", ['Compound', 'Positive', 'Neutral', 'Negative'], "The happier interface.")
 
 if __name__ == '__main__':
     main()
